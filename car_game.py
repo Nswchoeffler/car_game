bb928bbd5d4869d00757f9ae78ec79ae3ff17938
Nicholas Schoeffler <nwschoeffler@gmail.com>
2021-11-10 09:19:04 -0600

    finished

--- car_game.py
+++ car_game.py
@@ -1,9 +1,10 @@
+#imports
 import pygame, sys
 from pygame.locals import*
-import random
+import random, time
 
 #initialize programs
-pygame.init
+pygame.init()
 
 #FPS assigning
 FPS= 60 
@@ -20,6 +21,16 @@ white = (255,255,255)
 screen_width = 400
 screen_height = 600
 SPEED = 5
+score = 0
+
+#setting up fonts
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, black)
+
+background = pygame.image.load('AnimatedStreet.png')
+
+#setting up a white screen
 displaysurf = pygame.display.set_mode((screen_width,screen_height))
 displaysurf.fill(white)
 pygame.display.set_caption("car game")
@@ -33,13 +44,12 @@ class enemy(pygame.sprite.Sprite):
         self.rect.center = (random.randint(40,screen_width-40),0)
 
     def move(self):
+        global score
         self.rect.move_ip(0,SPEED)
-        if (self.rect.bottom> 600):
+        if (self.rect.bottom > 600):
+            score += 1
             self.rect.top = 0
-            self.rect.center = (random.randint(30,370),0)
-
-    def draw(self,surface):
-        surface.blit(self.image,self.rect)
+            self.rect.center = (random.randint(40,screen_width-40),0)
 
 #player class 
 class player(pygame.sprite.Sprite):
@@ -48,19 +58,13 @@ class player(pygame.sprite.Sprite):
         self.image = pygame.image.load("Player.png")
         self.rect = self.image.get_rect()
         self.rect.center = (160,520)
-    def move(self):
-        self.rect.move_ip(0,10)
-        if (self.rect.bottom> 600):
-            self.rect.top = 0
-            self.rect.center = (random.randint(30,370),0)
 
-    #key presses
-    def update(self):
+    def move(self):
         pressed_keys = pygame.key.get_pressed()
         #if pressed_keys[K_UP]:
             #self.rect.move_ip(0,-5)
-        #if pressed_keys[K_Down]:
-            #self.rect.move_ip(5,0)
+        #if pressed_keys[K_DOWN]:
+            #self.rect.move_ip(-5,0)
         if self.rect.left > 0:
             if pressed_keys[K_LEFT]:
                 self.rect.move_ip (-5,0)
@@ -68,8 +72,6 @@ class player(pygame.sprite.Sprite):
             if pressed_keys[K_RIGHT]:
                 self.rect.move_ip(5,0)
 
-    def draw(self,surface):
-        surface.blit(self.image, self.rect)
 
 #player and enemy 
 P1= player()
@@ -92,20 +94,37 @@ while True:
     #quit the game correctly
     for event in pygame.event.get():
         if event.type == inc_speed:
-            SPEED +=2
+            SPEED +=0.5
 
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
     #update the sprite and time
-    displaysurf.fill(white)
+    displaysurf.blit(background,(0,0))
+    scores = font_small.render(str(score),True, black)
+    displaysurf.blit(scores, (10,10))
 
     #moves and re_draws all sprites
     for entity in all_sprites:
-        #line 97
-    P1.draw(displaysurf)
-    E1.draw(displaysurf)
+        displaysurf.blit(entity.image,entity.rect)
+        entity.move()
+
+    #collision
+    if pygame.sprite.spritecollideany(P1, enemies):
+        pygame.mixer.Sound('crash.wav').play()
+        time.sleep(0.5)
+
+        displaysurf.fill(red)
+        displaysurf.blit(game_over,(30,250))
+
+        pygame.display.update()
+        for entity in all_sprites:
+            entity.kill()
+        time.sleep(2)
+        pygame.quit()
+        sys.exit()
+
 
     pygame.display.update()
     clock.tick(FPS)
